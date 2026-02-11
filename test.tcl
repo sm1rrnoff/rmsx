20645b421fd17ab659532d3b25c60878f1d83e4d
Finn Beruldsen <66322386+Finn2400@users.noreply.github.com>
2026-02-11 11:05:38 -0600

    Delete test.tcl

--- test.tcl
+++
@@ -1,6 +0,0 @@
-# This is a minimal tcl script for VMD
-puts "
---------------------------------------"
-puts "VMD is running this test.tcl script..."
-puts "It will close as soon as this script finishes."
-puts "--------------------------------------"
